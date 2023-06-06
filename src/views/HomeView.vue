f3f9eab85dcbdf669765b74ce6c8660e673e8900
Daniel Tesfagabr <d.tesfagabr@theonepoint.co.uk>
2023-06-06 11:18:34 +0100

    added login component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import LoginView from "@/views/LoginView.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <LoginView />
   </main>
 </template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div class="pt-16 justify-center">
+      <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
+      <form action="#" @submit.prevent="handleLogin">
+          <div class="overflow-hidden shadow sm:rounded-md max-w-sm max-auto text-left">
+              <div class="bg-white px-4 py-5 sm:p-6">
+                  <div>
+                      <input type="text" v-maska data-maska="###########" v-model="credentials.phone" name="phone" id="phone" placeholder="Enter phone number"
+                             class="mt-1 block w-full px-3 py-2 rounded-mb border border-gray-300 shadow-sm" />
+                  </div>
+
+              </div>
+
+              <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                  <button type="submit" @submit.prevent="handleLogin" class="inline-flex justify-center rounded-md border border-transparent bg-white py-2">Submit</button>
+              </div>
+
+          </div>
+      </form>
+
+
+  </div>
+</template>
+
+<script setup>
+import { vMaska } from "maska"
+import {reactive} from "vue";
+import axios from "axios";
+
+const credentials = reactive({
+    phone: null
+});
+
+const handleLogin = () => {
+    axios.post('http://127.0.0.1:8001/api/login', credentials)
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+            alert(error.response.data.message)
+        })
+}
+</script>
+
+<style>
+
+</style>
