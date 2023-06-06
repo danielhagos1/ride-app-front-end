87e8014d23df11c9e02ac739f1fa4393f5779eb2
Daniel Tesfagabr <d.tesfagabr@theonepoint.co.uk>
2023-06-06 13:12:19 +0100

    added code verification

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,46 +1,87 @@
-
 <template>
-  <div class="pt-16 justify-center">
-      <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-      <form action="#" @submit.prevent="handleLogin">
-          <div class="overflow-hidden shadow sm:rounded-md max-w-sm max-auto text-left">
-              <div class="bg-white px-4 py-5 sm:p-6">
-                  <div>
-                      <input type="text" v-maska data-maska="###########" v-model="credentials.phone" name="phone" id="phone" placeholder="Enter phone number"
-                             class="mt-1 block w-full px-3 py-2 rounded-mb border border-gray-300 shadow-sm" />
-                  </div>
+    <div class="pt-16 justify-center">
+        <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
 
-              </div>
+        <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm max-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="###########" v-model="credentials.phone" name="phone"
+                               id="phone" placeholder="Enter phone number"
+                               class="mt-1 block w-full px-3 py-2 rounded-mb border border-gray-300 shadow-sm"/>
+                    </div>
 
-              <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button type="submit" @submit.prevent="handleLogin" class="inline-flex justify-center rounded-md border border-transparent bg-white py-2">Submit</button>
-              </div>
+                </div>
 
-          </div>
-      </form>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" @submit.prevent="handleLogin"
+                            class="inline-flex justify-center rounded-md border border-transparent bg-white py-2">Continue
+                    </button>
+                </div>
+
+            </div>
+        </form>
+
+        <form v-else action="#" @submit.prevent="handleVerification">
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm max-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="######" v-model="credentials.login_code" name="login_code"
+                               id="login_code" placeholder="123456"
+                               class="mt-1 block w-full px-3 py-2 rounded-mb border border-gray-300 shadow-sm"/>
+                    </div>
+
+                </div>
+
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" @submit.prevent="handleVerification"
+                            class="inline-flex justify-center rounded-md border border-transparent bg-white py-2">Verify
+                    </button>
+                </div>
+
+            </div>
+        </form>
 
 
-  </div>
+    </div>
 </template>
 
 <script setup>
-import { vMaska } from "maska"
-import {reactive} from "vue";
+import {vMaska} from "maska"
+import {reactive, ref} from "vue";
 import axios from "axios";
 
 const credentials = reactive({
-    phone: null
+    phone: null,
+    login_code: null
 });
 
+const waitingOnVerification = ref(false)
+
 const handleLogin = () => {
-    axios.post('http://127.0.0.1:8001/api/login', credentials)
+    axios.post('http://127.0.0.1:8001/api/login', {
+        phone: credentials.phone
+    })
         .then((response) => {
             console.log(response.data)
+            waitingOnVerification.value = true
         })
         .catch((error) => {
             console.log(error)
             alert(error.response.data.message)
         })
+}
+
+const handleVerification = () => {
+    axios.post('http://127.0.0.1:8001/api/login/verify', {
+        phone: credentials.phone,
+        login_code: credentials.login_code,
+    }).then((response) => {
+        console.log(response.data) //auth token
+    }).catch((error) => {
+        console.log(error)
+        alert(error.response.data.message);
+    })
 }
 </script>
 
